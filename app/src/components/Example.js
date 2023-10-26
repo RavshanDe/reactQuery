49a57586314c38553e831207163f666c7c9c64f2
Ravshan <javascriptphyton@gmail.com>
2023-10-26 19:37:22 +0500

    QueryClient, QueryClientProvider, useQuery

--- app/src/components/Example.js
+++ app/src/components/Example.js
@@ -1,7 +1,25 @@
 import React from "react";
 import { useQuery } from "react-query";
+async function fetchPost() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return res.json();
+}
 const Example = () => {
-  return <div>Example</div>;
+  const { data, error, isLoading, isError } = useQuery("posts", fetchPost);
+  if (isLoading) {
+    return <h1>Yuklanmoqda...</h1>;
+  }
+  if (isError) {
+    return <pre>Yuklanishda xatolik bor... {error.message}</pre>;
+  }
+  return (
+    <>
+      <h1>Posts</h1>
+      {data.map((post, idx) => {
+        return <li key={idx}>{post.title}</li>;
+      })}
+    </>
+  );
 };
 
 export default Example;
